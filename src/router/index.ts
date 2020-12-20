d85dfbdc28eee4a0769b78e3b3bffaf94d84397b
chentian <chentian.ct@qq.com>
2020-12-20 14:05:54 +0800

    修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -96,7 +96,6 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
