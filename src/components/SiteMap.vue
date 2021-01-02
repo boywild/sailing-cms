43b268938e515efc55aac8e96cd97ad2981daf48
chentian <chentian.ct@qq.com>
2021-01-02 22:18:33 +0800

    修改

--- src/components/SiteMap.vue
+++ src/components/SiteMap.vue
@@ -5,7 +5,7 @@
       <div class="map-col" v-for="(map, index) in mapList" :key="index">
         <div class="m-c-title">{{ map.title }}</div>
         <div class="m-c-item" v-for="(col, colIndex) in map.items" :key="colIndex">
-          <router-link to="/">{{ col.name }}</router-link>
+          <router-link :to="{ name: col.link, query: { ...col.query } }">{{ col.name }}</router-link>
         </div>
       </div>
     </div>
@@ -27,40 +27,41 @@ export default defineComponent({
         {
           title: '航情',
           items: [
-            { name: '航情观察', link: '' },
-            { name: '市场交易', link: '' },
-            { name: '船舶管理', link: '' },
-            { name: '信德周报', link: '' }
+            { name: '航情观察', link: 'sailing', query: { type: '2', cat: '1' } },
+            { name: '市场交易', link: 'sailing', query: { type: '2', cat: '2' } },
+            { name: '船舶管理', link: 'sailing', query: { type: '2', cat: '3' } },
+            { name: '信德周报', link: 'sailing', query: { type: '2', cat: '4' } }
           ]
         },
         {
           title: '招聘',
           items: [
-            { name: '海员薪资', link: '' },
-            { name: '招聘求职', link: '' },
-            { name: '招生培训', link: '' },
-            { name: '航海院校', link: '' }
+            { name: '海员薪资', link: 'partner', query: { type: '3', cat: '1' } },
+            { name: '招聘求职', link: 'partner', query: { type: '3', cat: '3' } },
+            { name: '招生培训', link: 'partner', query: { type: '3', cat: '3' } },
+            { name: '航海院校', link: 'partner', query: { type: '3', cat: '4' } }
           ]
         },
         {
           title: '视频',
           items: [
-            { name: '航海生活', link: '' },
-            { name: '事故险情', link: '' },
-            { name: '港口风采', link: '' },
-            { name: '船舶技术', link: '' },
-            { name: '教育培训', link: '' }
+            { name: '航海生活', link: 'videolib', query: { type: '7', cat: '1' } },
+            { name: '事故险情', link: 'videolib', query: { type: '7', cat: '2' } },
+            { name: '港口风采', link: 'videolib', query: { type: '7', cat: '3' } },
+            { name: '船舶技术', link: 'videolib', query: { type: '7', cat: '4' } },
+            { name: '教育培训', link: 'videolib', query: { type: '7', cat: '5' } },
+            { name: '教育培训', link: 'videolib', query: { type: '7', cat: '6' } }
           ]
         },
         {
           title: '专题',
           items: [
-            { name: '海员频道', link: '' },
-            { name: '海上保安', link: '' },
-            { name: '航运企业', link: '' },
-            { name: '航届人物', link: '' },
-            { name: '疫情防控', link: '' },
-            { name: '天气播报', link: '' }
+            { name: '海员频道', link: 'topic', query: { type: '20' } },
+            { name: '海上保安', link: 'topic', query: { type: '20' } },
+            { name: '航运企业', link: 'topic', query: { type: '20' } },
+            { name: '航届人物', link: 'topic', query: { type: '20' } },
+            { name: '疫情防控', link: 'topic', query: { type: '20' } },
+            { name: '天气播报', link: 'topic', query: { type: '20' } }
           ]
         },
         // {
@@ -77,20 +78,20 @@ export default defineComponent({
         {
           title: '资料库',
           items: [
-            { name: '航海技术', link: '' },
-            { name: '轮机管理', link: '' },
-            { name: '海事法规', link: '' },
-            { name: 'PSC检查', link: '' },
-            { name: '海商海法', link: '' }
+            { name: '航海技术', link: 'home', query: {} },
+            { name: '轮机管理', link: 'home', query: {} },
+            { name: '海事法规', link: 'home', query: {} },
+            { name: 'PSC检查', link: 'home', query: {} },
+            { name: '海商海法', link: 'home', query: {} }
           ]
         },
         {
           title: '我们',
           items: [
-            { name: '关于我们', link: '' },
-            { name: '联系我们', link: '' },
-            { name: '免责声明', link: '' },
-            { name: '隐私政策', link: '' }
+            { name: '关于我们', link: 'home', query: {} },
+            { name: '联系我们', link: 'home', query: {} },
+            { name: '免责声明', link: 'home', query: {} },
+            { name: '隐私政策', link: 'home', query: {} }
           ]
         }
       ]
--- src/views/videolib/index.vue
+++ src/views/videolib/index.vue
@@ -6,8 +6,37 @@
     <template #content>
       <TradeComing class="mgb20" :content="state.videoHotList"></TradeComing>
       <Title name="要闻" subName="news"></Title>
-      <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
-      <a-button class="mgb20" block :loading="state.loadingMore" @click="getList" :disabled="state.pageNo > state.totalPage">
+      <a-tabs class="mgb20" @change="callback" :defaultActiveKey="state.tabDefaultActive">
+        <a-tab-pane key="0" tab="全部">
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+        <a-tab-pane key="1" tab="航海生活" force-render>
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="事故险情">
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="港口风采">
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+        <a-tab-pane key="4" tab="船舶技术">
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+        <a-tab-pane key="5" tab="教育培训">
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+        <a-tab-pane key="6" tab="航运报道">
+          <ArticleList class="mgb15" :list="state.videoList" :page-type="state.fromPage"></ArticleList>
+        </a-tab-pane>
+      </a-tabs>
+      <a-button
+        class="mgb20"
+        v-if="state.videoList.length"
+        block
+        :loading="state.loadingMore"
+        @click="getList"
+        :disabled="state.pageNo > state.totalPage"
+      >
         {{ state.pageNo > state.totalPage ? '没有更多了' : '加载更多' }}
       </a-button>
     </template>
@@ -40,33 +69,57 @@ export default defineComponent({
     const state = reactive({
       videoList: [],
       videoHotList: [],
+      tabDefaultActive: '',
       fromPage: '',
       pageNo: 1,
       totalPage: 1,
       showMoreBtn: false,
       loadingMore: false
     })
+    const route = useRoute()
+    const fromPage = route.query.type as string
+    const category = (route.query.cat as string) || '0'
+    state.fromPage = fromPage
+    state.tabDefaultActive = category
+
     const getList = () => {
       if (state.pageNo > state.totalPage) return
       state.loadingMore = true
-      article.getArticleList({ pageNo: state.pageNo, pageSize: 15, type: state.fromPage }).then(({ data = {} }) => {
-        state.videoList = state.videoList.concat(data.dataList)
-        state.totalPage = data.totalPage
-        state.loadingMore = false
-        state.pageNo += 1
-      })
+      article
+        .getArticleList({
+          pageNo: state.pageNo,
+          pageSize: 15,
+          type: state.fromPage,
+          categoryId: state.tabDefaultActive === '0' ? '' : state.tabDefaultActive
+        })
+        .then(({ data = {} }) => {
+          state.videoList = state.videoList.concat(data.dataList)
+          state.totalPage = data.totalPage
+          state.loadingMore = false
+          state.pageNo += 1
+        })
+    }
+    const reset = () => {
+      state.videoList = []
+      state.pageNo = 1
+      state.totalPage = 1
+      state.loadingMore = false
+    }
+    const callback = (key: string) => {
+      state.tabDefaultActive = key
+      reset()
+      getList()
     }
     onMounted(() => {
       // const { data = [] } = await Test.queryTaskStatus()
-      const route = useRoute()
-      const fromPage = route.query.type as string
-      state.fromPage = fromPage
+
       article.getArticleList({ pageNo: 1, pageSize: 9, type: fromPage }).then(({ data = {} }) => {
         state.videoHotList = data.dataList
       })
       getList()
     })
     return {
+      callback,
       state,
       getList
     }
