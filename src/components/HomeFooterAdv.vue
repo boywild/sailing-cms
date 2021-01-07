d86a695bf9d1849129b9caaca47edc9c687f0057
chentian <chentian@kayak.com.cn>
2021-01-07 21:15:54 +0800

    修改

---
+++ src/components/HomeFooterAdv.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home-cat-adv" v-if="img.show">
+    <SingleAdv height="118" :content="img.content"></SingleAdv>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import SingleAdv from './SingleAdv.vue'
+
+export default defineComponent({
+  name: 'HomeFooterAdv',
+  props: {
+    count: { type: Number, default: () => 0 },
+    imgList: { type: Array, default: () => [] }
+  },
+  components: { SingleAdv },
+  setup(props) {
+    const img = reactive({
+      show: false,
+      content: {}
+    })
+    console.info(props.imgList)
+    if ((props.count + 1) % 5 === 0) {
+      const index = (props.count + 1) / 5
+      img.content = (props.imgList[index - 1] as any) || {}
+      img.show = true
+    }
+    return {
+      img
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.single-adv {
+  cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,6 +35,43 @@
             <a-button v-else class="mgb20" block :loading="state.loadingMore" @click="getList" :disabled="state.pageNo > state.totalPage">
               {{ state.pageNo > state.totalPage ? '没有更多了' : '加载更多' }}
             </a-button>
+            <div class="category-content">
+              <div class="category-list">
+                <div v-for="(item, index) in column1" :key="index">
+                  <div class="category-item">
+                    <ImgLazy
+                      width="151"
+                      height="101"
+                      :src="hotAll[item.field] && hotAll[item.field][0] && hotAll[item.field][0].image"
+                    ></ImgLazy>
+                    <div class="category-info">
+                      <div class="category-top">
+                        <router-link target="_blank" :to="{ name: item.link, query: { type: item.type } }" class="c-name">{{
+                          item.name
+                        }}</router-link>
+                        <div class="c-comment">{{ hotAll[item.field] && hotAll[item.field][0] && hotAll[item.field][0].comments }}</div>
+                      </div>
+                      <router-link
+                        :to="{
+                          name: 'post',
+                          params: {
+                            fromPage: item.type,
+                            articleId: hotAll[item.field] && hotAll[item.field][0] && hotAll[item.field][0].articleId
+                          }
+                        }"
+                        class="category-title"
+                      >
+                        {{ hotAll[item.field] && hotAll[item.field][0] && hotAll[item.field][0].title }}
+                      </router-link>
+                      <div class="category-time">
+                        信德海事{{ hotAll[item.field] && hotAll[item.field][0] && hotAll[item.field][0].createDate }}
+                      </div>
+                    </div>
+                  </div>
+                  <HomeFooterAdv :count="index" :img-list="state.adv4Img"></HomeFooterAdv>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -48,7 +85,7 @@
             { title: '集美大学电子电气员定向委培班联合招生', image: '/upload/home-adv4.png' }
           ]"
         ></Technology>
-        <TradeShow class="mgb15" before="活动" after="会展" :content="meetingNews"></TradeShow>
+        <TradeShow class="mgb15" before="活动" after="会展" :content="hotAll.hyi"></TradeShow>
         <div class="usually-tools mgb20">
           <div class="tools-title">
             <div class="txt">实用查询</div>
@@ -65,11 +102,29 @@
         </div>
         <ReadingTop class="mgb15" before="阅读" after="排行" :content="homeReadTop"></ReadingTop>
         <MultipleAdv height="92" :list="state.adv3Img"></MultipleAdv>
-        <HotBox class="mgb15" before="国内" after="热点" :main="inlandNews.slice(0, 2)" :list="inlandNews.slice(2)"></HotBox>
-        <ArticleToday class="mgb15" :content="internationalNews"></ArticleToday>
-        <HotBox class="mgb15" before="活动" after="热点" :main="activeHot.slice(0, 2)" :list="activeHot.slice(2)"></HotBox>
-        <HotBox class="mgb15" before="航情" after="热点" :main="sailingHot.slice(0, 2)" :list="sailingHot.slice(2)"></HotBox>
-        <FeatureBox before="专栏" after="分享" :content="professionNews"></FeatureBox>
+        <HotBox
+          class="mgb15"
+          before="国内"
+          after="热点"
+          :main="hotAll.gn && hotAll.gn.slice(0, 2)"
+          :list="hotAll.gn && hotAll.gn.slice(2)"
+        ></HotBox>
+        <ArticleToday class="mgb15" :content="hotAll.gj"></ArticleToday>
+        <HotBox
+          class="mgb15"
+          before="活动"
+          after="热点"
+          :main="hotAll.hd && hotAll.hd.slice(0, 2)"
+          :list="hotAll.hq && hotAll.hd.slice(2)"
+        ></HotBox>
+        <HotBox
+          class="mgb15"
+          before="航情"
+          after="热点"
+          :main="hotAll.hq && hotAll.hq.slice(0, 2)"
+          :list="hotAll.hq && hotAll.hq.slice(2)"
+        ></HotBox>
+        <FeatureBox before="专栏" after="分享" :content="hotAll.zl"></FeatureBox>
       </div>
     </template>
   </PageContent>
@@ -129,6 +184,7 @@ import ArticleToday from '@/components/ArticleToday.vue'
 import ReadingTop from '@/components/ReadingTop.vue'
 import SingleAdv from '@/components/SingleAdv.vue'
 import MultipleAdv from '@/components/MultipleAdv.vue'
+import HomeFooterAdv from '@/components/HomeFooterAdv.vue'
 // import Test from '@/api/test'
 import article from '@/api/article'
 SwiperCore.use([Pagination, Autoplay])
@@ -149,28 +205,24 @@ export default defineComponent({
     ArticleToday,
     ReadingTop,
     SingleAdv,
-    MultipleAdv
+    MultipleAdv,
+    HomeFooterAdv
   },
   setup() {
-    const topArticles = ref([])
     const homeBanner = ref([]) // 首页banner
     const homeNews = ref([]) // 首页新闻
     const homeReadTop = ref([]) // 首页阅读排行
-    const sailingHot = ref([]) // 航情热点
-    const meetingNews = ref([]) // 活动会展
-    const activeHot = ref([]) // 活动热点
-    const professionNews = ref([]) // 专栏分享
-    const inlandNews = ref([]) // 国内热点
-    const internationalNews = ref([]) // 国际风云
     const state = reactive({
       homeNews: [],
-      adv1Img: '',
-      adv2Img: '',
-      adv3Img: [],
+      adv1Img: '', // 顶部
+      adv2Img: '', // 中部
+      adv3Img: [], // 右侧
+      adv4Img: [], // 底部
       pageNo: 1,
       totalPage: 1,
       loadingMore: false
     })
+    const hotAll: { [key: string]: any } = reactive({})
     const getList = () => {
       if (state.pageNo > state.totalPage) return
       state.loadingMore = true
@@ -187,59 +239,94 @@ export default defineComponent({
       const { data: adv1 } = await article.advList(fromPage, '1')
       const { data: adv2 } = await article.advList(fromPage, '2')
       const { data: adv3 } = await article.advList(fromPage, '4')
-      console.log(adv1)
+      const { data: adv4 } = await article.advList(fromPage, '3')
+      console.log(adv4.dataList)
       state.adv1Img = adv1.dataList[0]
       state.adv2Img = adv2.dataList[0]
       state.adv3Img = adv3.dataList
+      state.adv4Img = adv4.dataList
+    }
+    const column1 = [
+      { name: '航情', type: '2', hot: '1', pageSize: 7, link: 'sailing', field: 'hq' },
+      { name: '招聘', type: '3', hot: '1', pageSize: 7, link: 'partner', field: 'zp' },
+      { name: '活动', type: '4', hot: '1', pageSize: 7, link: 'activity', field: 'hd' },
+      { name: '专栏', type: '5', hot: '1', pageSize: 7, link: 'profession', field: 'zl' },
+      { name: '图片', type: '6', hot: '1', pageSize: 7, link: 'imagelib', field: 'tp' },
+      { name: '视频', type: '7', hot: '1', pageSize: 7, link: 'videolib', field: 'sp' },
+      { name: '造船', type: '8', hot: '1', pageSize: 7, link: 'article', field: 'zc' },
+      { name: '交易', type: '9', hot: '1', pageSize: 7, link: 'article', field: 'jy' },
+      { name: '港口', type: '10', hot: '1', pageSize: 7, link: 'article', field: 'gk' },
+      { name: '海员', type: '11', hot: '1', pageSize: 7, link: 'article', field: 'hy' },
+      { name: '企业', type: '12', hot: '1', pageSize: 7, link: 'article', field: 'qy' },
+      { name: '人物', type: '13', hot: '1', pageSize: 7, link: 'article', field: 'rw' },
+      { name: '技术', type: '14', hot: '1', pageSize: 7, link: 'article', field: 'js' },
+      { name: '安全', type: '15', hot: '1', pageSize: 7, link: 'article', field: 'aq' },
+      { name: '会议', type: '16', hot: '1', pageSize: 7, link: 'meeting', field: 'hyi' },
+      { name: '政策', type: '17', hot: '1', pageSize: 7, link: 'article', field: 'zce' },
+      { name: '金融', type: '18', hot: '1', pageSize: 7, link: 'article', field: 'jr' },
+      { name: '保险', type: '19', hot: '1', pageSize: 7, link: 'article', field: 'bx' },
+      { name: '专题', type: '20', hot: '1', pageSize: 7, link: 'topic', field: 'zt' },
+      { name: '领航', type: '21', hot: '1', pageSize: 7, link: 'kol', field: 'lh' },
+      { name: '学校', type: '22', hot: '1', pageSize: 7, link: 'article', field: 'xx' },
+      { name: 'ENGLISH', type: '23', hot: '1', pageSize: 7, link: 'home', field: 'en' },
+      { name: '国内', type: '24', hot: '1', pageSize: 7, link: 'article', field: 'gn' },
+      { name: '国际', type: '25', hot: '1', pageSize: 7, link: 'article', field: 'gj' }
+    ]
+
+    const getAllHot = () => {
+      const fetchArray = []
+      // const allHotData: Array<any> = []
+      const len = Math.ceil(column1.length / 5)
+      for (let i = 0; i < len; i++) {
+        const arr = column1.slice(i * 5, (i + 1) * 5)
+        fetchArray.push(
+          arr.map((item) => {
+            return {
+              ...item,
+              http: article.getArticleList({ pageNo: 1, ...item })
+            }
+          })
+        )
+      }
+      console.log(fetchArray)
+      fetchArray.forEach(async (item, index) => {
+        const http = item.map((ele) => ele.http)
+        const res = await Promise.all(http)
+        item.forEach((val, index) => {
+          hotAll[val.field] = res[index].data.dataList
+          console.log(hotAll)
+        })
+        // console.log(item, res)
+      })
     }
     onMounted(() => {
-      // const { data = [] } = await Test.queryTaskStatus()
       const route = useRoute()
       const allData = [
         { name: '首页banner', type: (route.query.type || '1') as string, hot: '1', pageSize: 5 },
         // { name: '首页新闻', type: (route.query.type || '1') as string, sortType: '1' },
         { name: '首页阅读排行', type: '1', sortType: '2', pageSize: 30 },
-        { name: '航情热点', type: '2', hot: '1', pageSize: 7 },
-        { name: '活动会展', type: '16', sortType: '1', pageSize: 10 },
-        { name: '活动热点', type: '4', hot: '1', pageSize: 7 },
-        { name: '专栏分享', type: '5', sortType: '1', pageSize: 5 },
-        { name: '国内热点', type: '24', hot: '1', pageSize: 7 },
-        { name: '国际风云', type: '25', hot: '1', pageSize: 7 }
+        { name: '活动会展', type: '16', sortType: '1', pageSize: 10 }
       ]
       getList()
-      // const { type = '1' } = route.query
-      // const { data = [] } = await article.getArticleList({ pageNo: 1, pageSize: 30, type: type as string })
       const fetchList = allData.map((ele) => article.getArticleList({ pageNo: 1, ...ele }))
       Promise.all(fetchList).then((res) => {
         const allDataList = res.map((ele) => ele.data.dataList || [])
         homeBanner.value = allDataList[0]
-        // homeNews.value = allDataList[1]
         homeReadTop.value = allDataList[1]
-        sailingHot.value = allDataList[2]
-        meetingNews.value = allDataList[3]
-        activeHot.value = allDataList[4]
-        professionNews.value = allDataList[5]
-        inlandNews.value = allDataList[6]
-        internationalNews.value = allDataList[7]
       })
       // console.log(data)
       // topArticles.value = data
       getAdv()
+      getAllHot()
     })
-    console.log(topArticles)
     return {
       state,
       getList,
-      topArticles,
       homeBanner,
       homeNews,
       homeReadTop,
-      sailingHot,
-      meetingNews,
-      activeHot,
-      professionNews,
-      inlandNews,
-      internationalNews
+      hotAll,
+      column1
     }
   },
   data() {
